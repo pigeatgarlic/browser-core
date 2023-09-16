d425fc897a08427e63e84a106f8ae43ac62c8041
pigeatgarlic <huyhoangdo0205@gmail.com>
2023-09-17 04:12:52 +0700

    run on mobile

--- hid/mobile.ts
+++ hid/mobile.ts
@@ -5,6 +5,7 @@ import { getOS, OS } from "../utils/platform";
 
 export class MobileTouch {
     private onGoingTouchs: Map<number,TouchData>
+    private events : string[] = []
 
 	private os : OS
     private disable : boolean
@@ -29,9 +30,7 @@ export class MobileTouch {
         videoElement.addEventListener('touchstart',     this.handleStart.bind(this));
         videoElement.addEventListener('touchend',       this.handleEnd.bind(this));
         videoElement.addEventListener('touchmove',      this.handleMove.bind(this));
-        this.SendFunc((new HIDMsg(EventCode.GamepadConnect,{
-            gamepad_id: "0",
-        }).ToString()))
+        this.ListenEvents()
     }
 
 
@@ -59,45 +58,44 @@ export class MobileTouch {
         }
     }
 
-    private async handle_swipe(touch: TouchData) : Promise<void>{
-        const now = new Date().getTime();
-		const deltaTime = now           - touch.startTime.getTime();
-		const deltaX    = touch.clientX - touch.touchStart.clientX;
-		const deltaY    = touch.clientY - touch.touchStart.clientY;
 
-		/* work out what the movement was */
-		if (deltaTime > thresholdTime) {
-			/* gesture too slow */
-			return;
-		} else {
-			if ((deltaX > thresholdDistance)&&(Math.abs(deltaY) < thresholdDistance)) {
-				// o.innerHTML = 'swipe right';
-			} else if ((-deltaX > thresholdDistance)&&(Math.abs(deltaY) < thresholdDistance)) {
-				// o.innerHTML = 'swipe left';
-			} else if ((deltaY > thresholdDistance)&&(Math.abs(deltaX) < thresholdDistance)) {
-				// o.innerHTML = 'swipe down';
-                for (let index = 0; index < 20; index++) {
-                    this.SendFunc((new HIDMsg(EventCode.MouseWheel,{
-                        deltaY: 120
-                    })).ToString());
+
+
+    private async ListenEvents () {
+        while (!this.disable) {
+            const first = this.events.pop()
+            if (first == "two_start") {
+                await new Promise(r => setTimeout(r,200))
+                const sec   = this.events.pop()
+                const third = this.events.pop()
+                if (sec == "short" && third == "short") {
+                    this.SendFunc((new HIDMsg(EventCode.MouseDown, { button: '2' })).ToString());
+                    this.SendFunc((new HIDMsg(EventCode.MouseUp  , { button: '2' })).ToString());
                 }
-			} else if ((-deltaY > thresholdDistance)&&(Math.abs(deltaX) < thresholdDistance)) {
-				// o.innerHTML = 'swipe up';
-                for (let index = 0; index < 20; index++) {
-                    setTimeout(() => {
-                        this.SendFunc((new HIDMsg(EventCode.MouseWheel,{
-                            deltaY: -120
-                        })).ToString());
-                    }, index * 30)
+            } else if (first == "short") {
+                await new Promise(r => setTimeout(r,400))
+                const sec = this.events.pop()
+                if (sec == "short")  {
+                    this.SendFunc((new HIDMsg(EventCode.MouseDown, { button: '0' })).ToString());
+                    this.SendFunc((new HIDMsg(EventCode.MouseUp  , { button: '0' })).ToString());
+                    this.SendFunc((new HIDMsg(EventCode.MouseDown, { button: '0' })).ToString());
+                    this.SendFunc((new HIDMsg(EventCode.MouseUp  , { button: '0' })).ToString());
+                } else if (sec == "long") {
+                    this.SendFunc((new HIDMsg(EventCode.MouseDown, { button: '0' })).ToString());
+                } else {
+                    this.SendFunc((new HIDMsg(EventCode.MouseDown, { button: '0' })).ToString());
+                    this.SendFunc((new HIDMsg(EventCode.MouseUp  , { button: '0' })).ToString());
                 }
-			} else {
-				// o.innerHTML = '';
-			}
-		}
+            } else
+                await new Promise(r => setTimeout(r,100))
+            
+        }
     }
 
 
     private handleStart = (evt: TouchEvent) => {
+        evt.preventDefault()
+
         if (this.disable) 
             return;
 
@@ -105,10 +103,14 @@ export class MobileTouch {
         for (let i = 0; i < touches.length; i++) {
 			const key = this.os == 'Android' ? touches[i].identifier : i;
 			this.onGoingTouchs.set(key, new TouchData(touches[i]));
-
         }
+
+        if (evt.touches.length == 2) 
+            this.events.push("two_start")
     };
     private handleEnd = (evt: TouchEvent) => {
+        evt.preventDefault()
+
         if (this.disable) 
             return;
 
@@ -116,75 +118,131 @@ export class MobileTouch {
         for (let i = 0; i < touches.length; i++) {
 			const key = this.os == 'Android' ? touches[i].identifier : i;
 			const touch = this.onGoingTouchs.get(key);
-            touch != null ? this.handle_swipe(touch) : null;
+            if(touch != null) 
+                this.handleScroll(touch) 
+            if (new Date().getTime() - touch.startTime.getTime() < 200)
+                this.events.push('short')
+
             this.onGoingTouchs.delete(key);
         }
+        
+
+        this.SendFunc((new HIDMsg(EventCode.MouseUp, { button: '0' })).ToString());
     };
 
     private handleMove = async (evt: TouchEvent) => {
+        evt.preventDefault()
+
         if (this.disable) 
             return;
 
         const touches = evt.touches;
         for (let i = 0; i < touches.length; i++) {
-			const key = this.os == 'Android' ? touches[i].identifier : i;
-            const touch = this.onGoingTouchs.get(key);
-			if(!touch) return
-            touch.clientX = touches[i].clientX;
-            touch.clientY = touches[i].clientY;
-            this.onGoingTouchs.set(key, touch);
+            const curr_touch = touches[i]
+            const identifier = curr_touch.identifier;
+
+            const prev_touch = this.onGoingTouchs.get(identifier);
+            if (prev_touch == null) 
+                continue;
+            
+            if (new Date().getTime() - prev_touch.startTime.getTime() > 200 && 
+                curr_touch.clientX - prev_touch.touchStart.clientX < 10 &&
+                curr_touch.clientY - prev_touch.touchStart.clientY < 10 &&
+               !prev_touch.doMove) {
+                prev_touch.doMove = true
+                this.events.push('long')
+            }
+
+            const diff = {
+                movementX: 3 * Math.round(curr_touch.clientX - prev_touch.clientX),
+                movementY: 3 * Math.round(curr_touch.clientY - prev_touch.clientY)
+            }
+
+            // one finger only
+            if (identifier == 0) {
+                let code = EventCode.MouseMoveRel
+                this.SendFunc((new HIDMsg(code, {
+                    dX: diff.movementX,
+                    dY: diff.movementY,
+                })).ToString());
+            }
+
+            prev_touch.copyFromTouch(curr_touch)
         }
 
-        if (this.onGoingTouchs.size != 2) {
+        this.handleSwipe()
+    };
+
+
+
+
+
+
+
+
+
+
+    private async handleSwipe() {
+        if (this.onGoingTouchs.size != 2) 
             return;
-        }
 
-        const firstFinger = this.onGoingTouchs.get(0);
+        const firstFinger  = this.onGoingTouchs.get(0);
         const secondFinger = this.onGoingTouchs.get(1);
 
         // Calculate the difference between the start and move coordinates
         const move = {
-            first: firstFinger.clientX - firstFinger.touchStart.clientX,
-            second: secondFinger.clientX - secondFinger.touchStart.clientX,
+            first  : firstFinger.clientX  - firstFinger.touchStart.clientX,
+            second : secondFinger.clientX - secondFinger.touchStart.clientX,
         };
         const distance = {
-            now: firstFinger.clientX - secondFinger.clientX,
-            prev:
-                firstFinger.touchStart.clientX -
-                secondFinger.touchStart.clientX,
+            now     : firstFinger.clientX            - secondFinger.clientX,
+            prev    : firstFinger.touchStart.clientX - secondFinger.touchStart.clientX,
         };
 
         // This threshold is device dependent as well as application specific
         const PINCH_THRESHOLD = document.documentElement.clientWidth / 10;
 
         // zoom
-        if (
-            !(
-                Math.abs(move.first) > PINCH_THRESHOLD &&
-                Math.abs(move.second) > PINCH_THRESHOLD
-            )
-        ) {
+        if ( !( Math.abs(move.first) > PINCH_THRESHOLD &&
+                Math.abs(move.second) > PINCH_THRESHOLD)) 
             return;
-        }
 
         // zoom in
-        if (
-            Math.abs(distance.now) > Math.abs(distance.prev) &&
-            !isFullscreen(this.video)
-        ) {	
+        if ( Math.abs(distance.now) > Math.abs(distance.prev) &&
+            !isFullscreen(this.video)) 	
             requestFullscreen();
-            return;
-        }
 
         // zoom out
-        if (
-            Math.abs(distance.now) < Math.abs(distance.prev) &&
-            isFullscreen(this.video)
-        ) {
-			try {
-				await document.exitFullscreen();
-            } catch (e) {}
-            return;
+        if ( Math.abs(distance.now) < Math.abs(distance.prev) &&
+            isFullscreen(this.video)) 
+            document.exitFullscreen().catch(e => {});
+    }
+
+    private async handleScroll(touch: TouchData) {
+        const now = new Date().getTime();
+		const deltaTime = now           - touch.startTime.getTime();
+		const deltaX    = touch.clientX - touch.touchStart.clientX;
+		const deltaY    = touch.clientY - touch.touchStart.clientY;
+
+		/* work out what the movement was */
+		if (deltaTime > thresholdTime) 
+			return;
+        else if ((deltaX > thresholdDistance)&&(Math.abs(deltaY) < thresholdDistance)) { // 'swipe right';
+            return
+        } else if ((-deltaX > thresholdDistance)&&(Math.abs(deltaY) < thresholdDistance)) { // 'swipe left';
+            return
+        } 
+
+        if ((deltaY > thresholdDistance)&&(Math.abs(deltaX) < thresholdDistance)) { // 'swipe down';
+            for (let index = 0; index < 10; index++) {
+                this.SendFunc((new HIDMsg(EventCode.MouseWheel,{ deltaY: 40 })).ToString());
+                await new Promise(r => setTimeout(r,30))
+            }
+        } else if ((-deltaY > thresholdDistance)&&(Math.abs(deltaX) < thresholdDistance)) { // 'swipe up';
+            for (let index = 0; index < 10; index++) {
+                this.SendFunc((new HIDMsg(EventCode.MouseWheel,{ deltaY: -40 })).ToString());
+                await new Promise(r => setTimeout(r,30))
+            }
         }
-    };
+    }
 }
