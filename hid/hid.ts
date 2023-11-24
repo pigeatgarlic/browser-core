b4e8c2c931c35005acf99aec16a50586489fce47
DearFlow-Founders <build.dearflow@gmail.com>
2023-11-24 12:03:10 +0700

    change shortcut

--- hid/hid.ts
+++ hid/hid.ts
@@ -95,7 +95,7 @@ export class HID {
          * shortcuts stuff
          */
         this.shortcuts = new Array<Shortcut>();
-        this.shortcuts.push(new Shortcut(ShortcutCode.Fullscreen,[KeyCode.Ctrl,KeyCode.Shift,KeyCode.P],requestFullscreen))
+        this.shortcuts.push(new Shortcut(ShortcutCode.Fullscreen, [KeyCode.Ctrl, KeyCode.Shift, KeyCode.F], requestFullscreen))
 
         /**
          * gamepad stuff
