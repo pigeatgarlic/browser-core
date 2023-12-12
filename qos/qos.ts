91153378b678647e1b5a424fc73334304e0e655c
pigeatgarlic <huyhoangdo0205@gmail.com>
2023-12-12 21:09:12 +0700

    increase loop period

--- qos/qos.ts
+++ qos/qos.ts
@@ -28,7 +28,7 @@ export class Adaptive {
         this.audioMetricCallback   = callback.audioMetricCallback   ;
         this.videoMetricCallback   = callback.videoMetricCallback   ;
 
-        this.loopNumber = setInterval(this.getConnectionStats.bind(this),period ?? 20)
+        this.loopNumber = setInterval(this.getConnectionStats.bind(this),period ?? 50)
     }
 
 
