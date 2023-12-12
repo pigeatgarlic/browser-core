a8f7e3c887a5540e5b7a9042f70ce80a8583af32
DearFlow-Founders <build.dearflow@gmail.com>
2023-12-12 21:11:55 +0700

    check afk

--- hid/hid.ts
+++ hid/hid.ts
@@ -8,6 +8,8 @@ import { convertJSKey } from "../utils/convert";
 
 const MOUSE_SPEED = 1.07
 
+const TIME_AFK = 10 * 60 * 1000
+
 export class HID {
     private prev_buttons : Map<number,boolean>;
     private prev_sliders : Map<number,number>;
@@ -21,6 +23,8 @@ export class HID {
     private disableKeyboard : boolean
     private disableMouse    : boolean
     private scancode        : boolean
+    private lastCheckHangUp: number
+    public afkStatus: 'low' | 'medium' | 'high'
 
     public setTouchMode (mode: 'gamepad' | 'trackpad' | 'mouse' | 'none') {
         if (mode == 'gamepad' || mode == 'trackpad')
@@ -60,13 +64,24 @@ export class HID {
 
         this.video = videoElement;
         this.SendFunc = Sendfunc;
+        this.lastCheckHangUp = new Date().getTime()
 
-        this.touch = new TouchHandler (videoElement,Sendfunc);
+        this.touch = new TouchHandler(videoElement, Sendfunc, this.lastCheckHangUp);
         this.Screen = new Screen();
         this.intervals = []
         this.pressing_keys = []
 
         this.disableKeyWhileFullscreen()
+
+
+        this.intervals.push(setInterval(() => {
+            const now = new Date().getTime()
+            const touchLastCheck = this.touch.lastCheckHangUp
+            //10  * 60 * 1000 
+            if (now - this.lastCheckHangUp >= TIME_AFK || now - touchLastCheck >= TIME_AFK) {
+                this.afkStatus = 'medium'
+            }
+        }, TIME_AFK))
         /**
          * video event
          */
@@ -223,7 +238,7 @@ export class HID {
                     index: index,
                     val: value
                 }).ToString()))
-
+                this.lastCheckHangUp = new Date().getTime()
                 this.prev_axis.set(index,value)
             })
         })
@@ -361,6 +376,8 @@ export class HID {
     private mouseButtonMovement(event: {clientX:number,clientY:number,movementX:number,movementY:number}){
         if (this.disableMouse) 
             return;
+
+        this.lastCheckHangUp = new Date().getTime()
 
         if (!this.relativeMouse) {
             this.elementConfig(this.video)
