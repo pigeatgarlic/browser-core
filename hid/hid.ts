3421fae52915e16a697adf0cdfd6c98bac283ecc
pigeatgarlic <huyhoangdo0205@gmail.com>
2024-01-19 10:36:59 +0700

    detect ptrlock

--- hid/hid.ts
+++ hid/hid.ts
@@ -88,7 +88,11 @@ export class HID {
         this.intervals.push(
             setInterval(
                 () =>
-                    (this.relativeMouse = document.pointerLockElement != null),
+                (this.relativeMouse =
+                    (document.pointerLockElement != null) ||
+                    ((document as any).mozPointerLockElement != null) ||
+                    ((document as any).webkitPointerLockElement != null)
+                ),
                 100
             )
         );
