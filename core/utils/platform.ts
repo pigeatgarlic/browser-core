2fec9fd5a5863d747d7d964bab3e89112fb372d3
pigeatgarlic <huyhoangdo0205@gmail.com>
2024-10-14 09:28:32 +0000

    format code

--- core/utils/platform.ts
+++ core/utils/platform.ts
@@ -79,14 +79,16 @@ export const isMobile = () => {
 };
 
 export function isIOS() {
-    return [
-        'iPad Simulator',
-        'iPhone Simulator',
-        'iPod Simulator',
-        'iPad',
-        'iPhone',
-        'iPod'
-    ].includes(navigator.platform)
+    return (
+        [
+            'iPad Simulator',
+            'iPhone Simulator',
+            'iPod Simulator',
+            'iPad',
+            'iPhone',
+            'iPod'
+        ].includes(navigator.platform) ||
         // iPad on iOS 13 detection
-        || (navigator.userAgent.includes("Mac") && "ontouchend" in document)
+        (navigator.userAgent.includes('Mac') && 'ontouchend' in document)
+    );
 }
