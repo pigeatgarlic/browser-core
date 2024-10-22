0bb3ce729955a431cd768b8e2e1ea765d78e6ab3
pigeatgarlic <huyhoangdo0205@gmail.com>
2024-10-22 08:02:45 +0000

    increase max bitrate

--- singleton/index.ts
+++ singleton/index.ts
@@ -4,8 +4,8 @@ export const SIZE = () =>
     CLIENT != null
         ? CLIENT.video.video.videoHeight * CLIENT.video.video.videoWidth
         : 1920 * 1080;
-export const MAX_BITRATE = () => (15000 / (1920 * 1080)) * SIZE();
-export const MIN_BITRATE = () => (1000 / (1920 * 1080)) * SIZE();
+export const MAX_BITRATE = () => Math.round((30000 / (1920 * 1080)) * SIZE());
+export const MIN_BITRATE = () => Math.round((500 / (1920 * 1080)) * SIZE());
 export const MAX_FRAMERATE = 120; //240
 export const MIN_FRAMERATE = 40;
 
