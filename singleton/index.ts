8140db9a875c76e04ec78db60b7a2a515bc72d49
carosaome <sieunhankiet@gmail.com>
2024-10-25 16:04:06 +0700

    limit bitrate 30 => 15

--- singleton/index.ts
+++ singleton/index.ts
@@ -4,7 +4,7 @@ export const SIZE = () =>
     CLIENT != null
         ? CLIENT.video.video.videoHeight * CLIENT.video.video.videoWidth
         : 1920 * 1080;
-export const MAX_BITRATE = () => Math.round((30000 / (1920 * 1080)) * SIZE());
+export const MAX_BITRATE = () => Math.round((15000 / (1920 * 1080)) * SIZE());
 export const MIN_BITRATE = () => Math.round((500 / (1920 * 1080)) * SIZE());
 export const MAX_FRAMERATE = 120; //240
 export const MIN_FRAMERATE = 40;
@@ -15,7 +15,7 @@ export const Assign = (client: RemoteDesktopClient) => {
     CLIENT = client;
 };
 
-export let PINGER = async () => {};
+export let PINGER = async () => { };
 export const SetPinger = (fun: () => Promise<void>) => {
     PINGER = fun;
 };
